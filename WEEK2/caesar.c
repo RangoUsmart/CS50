01280d4ee7007be2d97a546d4b4d38658008a039
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 16:37:32 +0000

    Wed, Dec 20, 2023, 6:37 PM +02:00

--- WEEK2/caesar.c
+++ WEEK2/caesar.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
-int main(int argc, int argv[]) {
+int main(int argc, char argv[]) {
     string name = get_string("Give me a string?\n");
     printf("Hello, %s!\n your key is %i", name, argv[1]);
     return 0;
