b979731ab1fe5c306b4bf67d31d698329d3f9ba9
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 21:12:45 +0000

    Wed, Dec 20, 2023, 11:12 PM +02:00

---
+++ WEEK2/Vigenère.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: %s <integer>\n", argv[0]);
+        return 1;
+    }
+    int shift = atoi(argv[1]);
+    string input = get_string("Give me a string:");
+    int length = strlen(input);
+    char output[length + 1];
+
+    for(int i=0; input[i]!='\0'; i++){
+        if(input[i]<='z'&&input[i]>='a'){
+            output[i]='a'+(input[i]-'a'+shift)%26;
+        }
+        else if(input[i]<='Z'&&input[i]>='A'){
+            output[i]=input[i]+shift;
+            output[i]='A'+(input[i]-'A'+shift)%26;
+        }
+        else{
+            output[i]=input[i];
+        }
+    }
+    output[length] = '\0';
+    printf("ciphertext: %s\n", output);
+    return 0;
+}
