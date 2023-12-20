78acadb15d1ebc91e2ffe34f92cafd1d381d6192
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 18:12:43 +0000

    Wed, Dec 20, 2023, 8:12 PM +02:00

--- WEEK2/caesar.c
+++ WEEK2/caesar.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     // char test3='a';
     // char test4='z';
 
-    input = get_string("Give me a string?\n");
+    string input = get_string"Give me a string\n");
     int length = strlen(input);
     char output[length + 1];
 
