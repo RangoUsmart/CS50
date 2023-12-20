8e2a6859febf4249f271ccf8c9559b354c6ec53d
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 18:15:16 +0000

    Wed, Dec 20, 2023, 8:15 PM +02:00

--- WEEK2/caesar.c
+++ WEEK2/caesar.c
@@ -38,6 +38,6 @@ int main(int argc, char *argv[]) {
         // printf("%c__%c \n",test3, test4);
         // printf("%d__%d \n",test3, test4);
     }
-    printf("ciphertext: \n", argv[0]);
+    printf("ciphertext: %s \n", argv[0]);
     return 0;
 }
