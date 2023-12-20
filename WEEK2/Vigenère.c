feb21e187cc912b06469efbdfb35cbee05678185
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 22:08:55 +0000

    Thu, Dec 21, 2023, 12:08 AM +02:00

--- WEEK2/Vigenère.c
+++ WEEK2/Vigenère.c
@@ -16,29 +16,10 @@ void typeofarg(char *argv[]){
 }
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
-        printf("Usage: %s <integer>\n", argv[0]);
-        return 1;
-    }
-    else if()
-    int shift = atoi(argv[1]);
     string input = get_string("Give me a string:");
+    typeofarg(input);
     int length = strlen(input);
     char output[length + 1];
-
-    for(int i=0; input[i]!='\0'; i++){
-        if(input[i]<='z'&&input[i]>='a'){
-            output[i]='a'+(input[i]-'a'+shift)%26;
-        }
-        else if(input[i]<='Z'&&input[i]>='A'){
-            output[i]=input[i]+shift;
-            output[i]='A'+(input[i]-'A'+shift)%26;
-        }
-        else{
-            output[i]=input[i];
-        }
-    }
-    output[length] = '\0';
     printf("ciphertext: %s\n", output);
     return 0;
 }
