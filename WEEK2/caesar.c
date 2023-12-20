0e72130ed2ce3f694204ed31e00a038c5fced8cb
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 20:01:43 +0000

    Wed, Dec 20, 2023, 10:01 PM +02:00

--- WEEK2/caesar.c
+++ WEEK2/caesar.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
     for(int i=0; input[i]!='\0'; i++){
         if(input[i]<='z'&&input[i]>='a'){
             printf("%d \n", input[i]);
-            printf("%d \n", shift);
+            // printf("%d \n", shift);
             output[i]=input[i]+shift;
             printf("%d \n", output[i]);
             if(output[i]>'z'){
