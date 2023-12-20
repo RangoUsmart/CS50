69d50d905184a6887af7ab7f81781e3d7c9fa83e
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 19:03:28 +0000

    Wed, Dec 20, 2023, 9:03 PM +02:00

--- WEEK2/caesar.c
+++ WEEK2/caesar.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
         output[i]=input[i]+shift;
         printf("%d \n", output[i]);
         if(output[i]>'z'){
-            output[i]='a'+output[i]-'z';
+            output[i]='a'+output[i]-'z'-1;
         }
         // else{
         //     output[i]=input[i];
