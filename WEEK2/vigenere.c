e9ab147f204ca3c1f4cb6dc95072d9f2094b3710
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 23:23:33 +0000

    Thu, Dec 21, 2023, 1:23 AM +02:00

--- WEEK2/vigenere.c
+++ WEEK2/vigenere.c
@@ -39,10 +39,12 @@ int main(int argc, char *argv[]) {
         key[i]=getkey(argv[1][i]);
         // printf("%d \n",getkey(input[i]));
         }
+    
+
+    for (int i = 0; i < length; i++) {
+        printf("_%d ", key[i]);
     }
-
-    printf("_%d \n", key);
-
+}
 
 
     // for(int i=0; input[i]!='\0'; i++){
