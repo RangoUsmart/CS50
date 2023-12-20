a6927880ac6603b7a17510fd653cc7b3ea3a01ef
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 16:12:00 +0000

    Wed, Dec 20, 2023, 6:12 PM +02:00

--- WEEK2/Caesar.c
+++ WEEK2/Caesar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(char) {
+int main(int argc, int argv[]) {
     string name = get_string("Give me a string?\n");
     printf("Hello, %s!\n", name);
     return 0;
