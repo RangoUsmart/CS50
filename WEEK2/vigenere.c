bfdea4ca336ffc707ec79284e1f579774d930f83
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-20 22:11:44 +0000

    Thu, Dec 21, 2023, 12:11 AM +02:00

--- WEEK2/vigenere.c
+++ WEEK2/vigenere.c
@@ -4,16 +4,19 @@
 #include <string.h>
 #include <ctype.h>
 
-int typeofarg(char *argv[]){
-    for(int i=0; argv[i]!='\0'; i++){
-        if(input[i]<='z'&&input[i]>='a'||input[i]<='Z'&&input[i]>='A'){
-             return 0;
-        }else{
-            printf("Usage: %s <integer>\n", argv[0]);
-            return 1;
-        }
+int typeofarg(char string[]) {
+    for (int i = 1; argv[i] != NULL; i++) { // Починаємо з індексу 1, бо argv[0] - це назва програми
+
+            if ((argv[i][j] <= 'z' && argv[i][j] >= 'a') || (argv[i][j] <= 'Z' && argv[i][j] >= 'A')) {
+                continue; // Продовжити перевірку наступного символу у цьому аргументі
+            } else {
+                printf("Usage: %s <integer>\n", argv[0]);
+                return 1;
+            }
     }
+    return 0; // Якщо всі символи у всіх аргументах - це букви, повертаємо 0
 }
+
 int main(int argc, char *argv[]) {
 
     string input = get_string("Give me a string:");
