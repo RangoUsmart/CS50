320d5441d3d98327117752f8377abb4afcbc2ae8
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2023-12-21 02:49:11 +0000

    Thu, Dec 21, 2023, 4:49 AM +02:00

--- WEEK2/vigenere.c
+++ WEEK2/vigenere.c
@@ -8,10 +8,8 @@ void typeofarg(char string[]) {
     for (int i = 0; string[i] != '\0'; i++) {
         if ((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z')) {
             printf("_%c\n", string[i]);
-            return 0;
         } else {
             printf("Usage: ./vigenere keyword\n");
-            return 1;
         }
     }
 }
