4b2cf933a401b22f01025d4a39537a4e612774a3
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 21:57:03 +0000

    Wed, Jan 3, 2024, 11:57 PM +02:00

--- WEEK6/crack.py
+++ WEEK6/crack.py
@@ -2,10 +2,12 @@ from crypt import crypt
 from sys import argv
 
 if len(argv) == 2:
+    laterS = 'abcdefghijklmnopqrstuvwxyz'
 
     hashtext=argv[1]
     print(hashtext)
-    
+    salt = hashtext[:2]
+
 else:
     print("USage: python crack.py hash")
     exit(1)
