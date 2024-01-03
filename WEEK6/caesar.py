9a2b205c35b0ef779fac0ae11dbb98fdb29bf4b2
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 20:51:04 +0000

    Wed, Jan 3, 2024, 10:51 PM +02:00

--- WEEK6/caesar.py
+++ WEEK6/caesar.py
@@ -8,11 +8,11 @@ def change(txt,arg1): #, par
     diferent=[]
     for char in txt:
         newchar=ord(char)+int(arg1)
-        if (ord("a")<=ord(char)<=ord("z")):
+        if (ord("a")<=ord(char)<=ord("z") or ord("A")<=ord(char)<=ord("Z")):
             if(newchar>ord("z")):
                 newchar=newchar-ord("z")+ord("a")-1
             elif(newchar>ord("Z")):
-                newchar=newchar-ord("z")+ord("a")-1
+                newchar=newchar-ord("Z")+ord("A")-1
             diferent.append(newchar)
         else:
             diferent.append(ord(char))
