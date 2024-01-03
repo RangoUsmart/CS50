d1d381c0f0c1781d8db00a28baf360292261fdf6
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 13:08:44 +0000

    Wed, Jan 3, 2024, 3:08 PM +02:00

--- WEEK6/cash.py
+++ WEEK6/cash.py
@@ -1,18 +1,19 @@
-# import two functions
 from cs50 import get_float
-# prompt user for an ampunt of change and check its validity
-cash=[25, 10, 5, 1]
-while True :
-    count=0
-    change = get_float("change owed :")
-    if change < 0 :
-        break
-    change=round(change*100)
 
-    for i in cash:
-        # print(i)
-        while change//i==1:
-            # print(i)
-            count=count+1
-            change = change-i
-        print(count)
+cash = [25, 10, 5, 1]
+
+while True:
+    count = 0
+    change = get_float("Change owed: ")
+
+    if change < 0:
+        break
+
+    cents = round(change * 100)
+
+    for coin in cash:
+        count += cents // coin
+        cents %= coin
+
+    print(count)
+    break
