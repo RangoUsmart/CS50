99c72846511ebce0f95a5482103209d24c4d1011
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 16:28:04 +0000

    Wed, Jan 3, 2024, 6:28 PM +02:00

--- WEEK6/credit.py
+++ WEEK6/credit.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 while True:
     try:
         number = get_int("Введіть ціле число: ")
-        number = int(user_input)
+        # number = int(user_input)
         if 12 < number < 17:
             print("Ваше число:", number)
             break
