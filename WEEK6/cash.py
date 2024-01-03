309ca52bcd3563fb5cc1ab84ffb734248c51f412
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 12:48:42 +0000

    Wed, Jan 3, 2024, 2:48 PM +02:00

--- WEEK6/cash.py
+++ WEEK6/cash.py
@@ -1,20 +1,17 @@
 # import two functions
 from cs50 import get_float
-
-count=0
 # prompt user for an ampunt of change and check its validity
 cash=[25, 10, 5, 1]
 while True :
-
+    count=0
     change = get_float("change owed :")
     if change < 0 :
         break
-
     change=round(change*100)
 
     for i in cash:
         print(i)
-        while change%i==0:
+        while change%i==1:
             count+=1
             change = change-i
         print(count)
