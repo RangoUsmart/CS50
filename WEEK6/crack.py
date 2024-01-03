ade9978b40c4b02e24e74d78c0ec315225cf81e6
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 22:17:54 +0000

    Thu, Jan 4, 2024, 12:17 AM +02:00

--- WEEK6/crack.py
+++ WEEK6/crack.py
@@ -1,8 +1,14 @@
 from crypt import crypt
 from sys import argv
 
-def generator(dict, size):
-    for
+def find_word(letters, length, target_word):
+    word = ''
+    for _ in range(length):
+        for letter in letters:
+            new_word = word + letter
+            if new_word == target_word:
+                return new_word
+    return None
 
 if len(argv) == 2:
     laterS = 'abcdefghijklmnopqrstuvwxyz'
@@ -12,6 +18,7 @@ if len(argv) == 2:
     print(hashtext)
     salt = hashtext[:2]
 
+    tri1=find_word(dict, i, hashtext)
 
 
 else:
