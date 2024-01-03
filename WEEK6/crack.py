85827564e3b11dc1e0ec77c43ca871e75b6015af
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 21:40:05 +0000

    Wed, Jan 3, 2024, 11:40 PM +02:00

--- WEEK6/crack.py
+++ WEEK6/crack.py
@@ -2,7 +2,10 @@ from crypt import crypt
 from sys import argv
 
 if len(argv) == 2:
-    
+
+    hashtext=sys.argv[1]
+    print(hashtext)
+
 else:
     print("USage: python crack.py hash")
     exit(1)
