1985cf9a955786cee19cf0c2febf3dbdf22a929c
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 22:00:03 +0000

    Thu, Jan 4, 2024, 12:00 AM +02:00

--- WEEK6/crack.py
+++ WEEK6/crack.py
@@ -1,13 +1,18 @@
 from crypt import crypt
 from sys import argv
 
+def generator(dict, size):
+    for
+
 if len(argv) == 2:
     laterS = 'abcdefghijklmnopqrstuvwxyz'
-    laterL = 'abcdefghijklmnopqrstuvwxyz'
+    laterL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     hashtext=argv[1]
     print(hashtext)
     salt = hashtext[:2]
+
+
 
 else:
     print("USage: python crack.py hash")
