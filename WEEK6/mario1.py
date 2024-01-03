351bc066cf687d17a052480e234df8978f2d6231
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 09:40:13 +0000

    Wed, Jan 3, 2024, 11:40 AM +02:00

--- WEEK6/mario1.py
+++ WEEK6/mario1.py
@@ -1,9 +1,10 @@
-number = int(input("Введіть ціле число: "))
-if 0<number<9:
-    for i in range(1, number+1):
-        form=number-i
-        row=(" "*form)+("#"*i)
-        print(row)
+number = input("Введіть ціле число: ")
+while int(number):
+    if 0<number<9:
+        for i in range(1, number+1):
+            form=number-i
+            row=(" "*form)+("#"*i)
+            print(row)
 else:
-    exit()
+    break
 
