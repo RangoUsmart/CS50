6f3b0770e40d70a2b0705ca6cdaa8c840902c75d
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 22:20:54 +0000

    Thu, Jan 4, 2024, 12:20 AM +02:00

--- WEEK6/crack.py
+++ WEEK6/crack.py
@@ -1,26 +1,25 @@
 from crypt import crypt
 from sys import argv
 
-def find_word(letters, length, target_word):
+def find_word(letters, length, target_word, key):
     word = ''
     for _ in range(length):
         for letter in letters:
             new_word = word + letter
-            if new_word == target_word:
-                return new_word
+            if crypt(new_word, key) == target_word:
+            # if new_word == target_word:
+                print new_word
     return None
 
 if len(argv) == 2:
     laterS = 'abcdefghijklmnopqrstuvwxyz'
     laterL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+    diction=
     hashtext=argv[1]
     print(hashtext)
     salt = hashtext[:2]
-
-    tri1=find_word(dict, i, hashtext)
-
-
+    for i in range(1,5)
+        tri1=find_word(dict, i, hashtext, salt)
 else:
     print("USage: python crack.py hash")
     exit(1)
