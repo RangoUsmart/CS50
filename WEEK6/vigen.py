e965431bfc1015180cbbfa9dcb9617a5843c41f4
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 22:26:55 +0000

    Thu, Jan 4, 2024, 12:26 AM +02:00

---
+++ WEEK6/vigen.py
@@ -0,0 +1,36 @@
+# from cs50 import get_int
+import sys
+
+def getString():
+    intext=input("Введіть текст: ")
+    return intext
+def change(txt,arg1): #, par
+    diferent=[]
+    for char in txt:
+        newchar=ord(char)+int(arg1)%26
+        if (ord("a")<=ord(char)<=ord("z")):
+            if(newchar>ord("z")):
+                newchar=newchar-ord("z")+ord("a")-1
+                diferent.append(newchar)
+            else:
+                diferent.append(newchar)
+        elif (ord("A")<=ord(char)<=ord("Z")):
+            if(newchar>ord("Z")):
+                newchar=newchar-ord("Z")+ord("A")-1
+                diferent.append(newchar)
+            else:
+                diferent.append(newchar)
+        else:
+             diferent.append(ord(char))
+    text = ''.join(chr(code) for code in diferent)
+    text = "ciphertext: "+text
+    return text
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python caesar.py k")
+        sys.exit(1)  # Завершення програми з кодом виходу 1
+
+    text = getString()
+    res = change(text, sys.argv[1])
+    print(res)
