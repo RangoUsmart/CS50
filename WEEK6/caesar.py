f53817522edc8c78ef2991940b547cbbe4098a0d
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 19:31:41 +0000

    Wed, Jan 3, 2024, 9:31 PM +02:00

--- WEEK6/caesar.py
+++ WEEK6/caesar.py
@@ -6,11 +6,5 @@ def getString():
 
 if __name__ == "__main__":
     arguments = sys.argv
-
-    # Перший аргумент (arguments[0]) - назва скрипта, тому перший аргумент зазвичай починається з індексу 1
-    # Виведення аргументів
-    print("Аргументи командного рядка:", arguments)
-
-    # Доступ до окремих аргументів
-    if len(arguments) > 1:
-        print("Перший аргумент:", arguments[1])
+    res=change(arguments[1])
+    print(res)
