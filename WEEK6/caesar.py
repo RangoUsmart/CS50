205e1119c6f6afaa7b734a971c7a7f5400ee18c9
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 20:05:28 +0000

    Wed, Jan 3, 2024, 10:05 PM +02:00

--- WEEK6/caesar.py
+++ WEEK6/caesar.py
@@ -7,13 +7,15 @@ def getString():
 def change(txt,arg1): #, par
     diferent=[]
     for char in txt
-        diferent.aord("char")+int(arg1)
-    for ckar in (ord("a"), ord("z"))
-    print("ASCII-коди символів:", ascii_list)
+        diferent.append(ord(char)+int(arg1))
+    # for ckar in (ord("a"), ord("z"))
+    # print("ASCII-коди символів:", ascii_list)
+    text = ''.join(chr(code) for code in diferent)
+    return text
 
 
 if __name__ == "__main__":
     text=getString()
     # arguments = sys.argv
     res=change(text) #, arguments[1]
-    # print(res)
+    print(res)
