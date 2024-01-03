23e5933261c90d9b16980286d37659386ae3a8bf
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 10:04:17 +0000

    Wed, Jan 3, 2024, 12:04 PM +02:00

---
+++ WEEK6/cash.py
@@ -0,0 +1,17 @@
+while True:
+    number = input("Введіть ціле число: ")
+    if not number.isdigit():
+            number
+    else:
+
+        if 0<int(number)<9:
+            for i in range(1, int(number)+1):
+                form=(int(number)-i)
+                row=(" "*form)+("#"*i)
+                midd=(" "*2)
+                row2=("#"*i)
+                print(row+midd+row2)
+            break
+        else:
+             number
+
