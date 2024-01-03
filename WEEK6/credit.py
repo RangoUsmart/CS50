671ee10d49eadc88a3978c0b2cfb8fb9e6f9b587
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 18:04:07 +0000

    Wed, Jan 3, 2024, 8:04 PM +02:00

--- WEEK6/credit.py
+++ WEEK6/credit.py
@@ -7,13 +7,14 @@ while True:
             summ=0
             param1=number[-2::-2]
             for i in param1:
+                print(i)
                 dub=int(i)*2
                 print(dub)
                 if dub>9:
                     while dub > 0:
                         digit = dub % 10
                         summ=summ+digit*2
-                        print(f"{digit} --")
+                        # print(f"{digit} --")
                         dub //= 10
                 else:
                     summ=summ+dub
@@ -24,7 +25,7 @@ while True:
             for i in param2:
                 summ=summ+int(i)
 
-            print(summ)
+            # print(summ)
         else:
             print("Число не знаходиться у діапазоні від 13 до 16 включно.")
     except ValueError:
