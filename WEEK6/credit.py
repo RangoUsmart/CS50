4c2d0cda68f2b3482aaa03a039ce585c4ab869e6
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-03 16:21:31 +0000

    Wed, Jan 3, 2024, 6:21 PM +02:00

--- WEEK6/credit.py
+++ WEEK6/credit.py
@@ -1,13 +1,10 @@
 from cs50 import get_int
-
 while True:
-    number = input("Введіть ціле число: ")
-    if not number.isdigit():
-            number
-    else:
-        if 13<int(number)<17:
-            for i in number:
-                print(number)
-            break
-        else:
-             number
+    try:
+        number = get_int("Введіть ціле число: ")
+        print("Ваше число:", number)
+        break  # Якщо користувач ввів число, вийти з циклу
+    except ValueError:
+        print("Це не число. Спробуйте ще раз.")
+
+print("Введене число:", number)
