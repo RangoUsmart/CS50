631e5d75359ee6dbaee8058898a9fdd410f20a90
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-04 21:34:22 +0000

    Thu, Jan 4, 2024, 11:34 PM +02:00

--- WEEK7/similarities/helpers.py
+++ WEEK7/similarities/helpers.py
@@ -1,15 +1,21 @@
 def lines(a, b):
     """Return lines in both a and b"""
     list =[]
-    len=0
+    len=[]
     a_split = a.split("\n")
     b_split = b.split("\n")
     a_split_len=len(a_split)
     b_split_len=len(b_split)
     if (a_split_len>=b_split_len):
         len=a_split
+    else:
+        len=b_split
 
-    # TODO
+    for рядок in рядки_1:
+        if рядок in рядки_2:
+            print(f"Рядок '{рядок}' присутній у обох наборах")
+        else:
+            print(f"Рядок '{рядок}' відсутній у другому наборі рядків")
     return []
 
 
