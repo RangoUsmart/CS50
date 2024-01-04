3253dc22abf53eedd4a31060c247e500fb874209
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-04 21:32:32 +0000

    Thu, Jan 4, 2024, 11:32 PM +02:00

--- WEEK7/similarities/helpers.py
+++ WEEK7/similarities/helpers.py
@@ -1,12 +1,14 @@
 def lines(a, b):
     """Return lines in both a and b"""
     list =[]
+    len=0
     a_split = a.split("\n")
     b_split = b.split("\n")
     a_split_len=len(a_split)
     b_split_len=len(b_split)
-    if (a_split_len>b_split_len):
-        
+    if (a_split_len>=b_split_len):
+        len=a_split
+
     # TODO
     return []
 
