a04ad5a449c883f02d10778bc99d27ed93cd90a8
Pavlo Cvetkov <121953443+RangoUsmart@users.noreply.github.com>
2024-01-04 22:04:54 +0000

    Fri, Jan 5, 2024, 12:04 AM +02:00

--- WEEK7/similarities/helpers.py
+++ WEEK7/similarities/helpers.py
@@ -1,13 +1,16 @@
 def lines(a, b):
     """Return lines in both a and b"""
     m_list =[]
+    new_list=[]
     a_split = a.split("\n")
     b_split = b.split("\n")
 
     for i in min(a_split, b_split, key=len):
         if i in max(a_split, b_split, key=len):
             m_list.append(i)
-    m_list=list(set(m_list))
+    for element in m_list:
+        if element not in new_list:
+            new_list.append(element)
     return m_list
 
 
